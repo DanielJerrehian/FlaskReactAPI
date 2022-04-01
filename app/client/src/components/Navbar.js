ce551c5d7adabe617748f5eefe9a904fc60f2b25
Daniel Jerrehian <d.jerrehian@kigroup.de>
2022-04-01 09:37:32 +0200

    mui changes

--- app/client/src/components/Navbar.js
+++ app/client/src/components/Navbar.js
@@ -1,15 +1,35 @@
 import React from "react"
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Box from "@mui/material/Box";
+import MenuItem from "@mui/material/MenuItem";
 import {NavLink} from "react-router-dom";
+
 
 
 function Navbar() {
     return (
-        <div className="navbar">
-            <NavLink to="/" className="navbar-link">Home</NavLink>
-            <NavLink to="/about" className="navbar-link">About</NavLink>
-            <NavLink to="/users" className="navbar-link">Users</NavLink>
-            <NavLink to="/color" className="navbar-link">Top Color</NavLink>
-        </div>
+        <AppBar color="secondary" position="static">
+            <Container maxWidth="xl">
+                <Toolbar>
+                    <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                        <MenuItem>
+                            <NavLink to="/" className="navbar-link">Home</NavLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <NavLink to="/about" className="navbar-link">About</NavLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <NavLink to="/users" className="navbar-link">Users</NavLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <NavLink to="/color" className="navbar-link">Top Color</NavLink>
+                        </MenuItem>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
 
