5ac14960c3fcd600088c137dc6d397d389d2d50e
Daniel Jerrehian <d.jerrehian@kigroup.de>
2022-04-12 15:58:31 +0200

    "removed comments in models.py"

--- app/client/src/components/UserDetails.js
+++ app/client/src/components/UserDetails.js
@@ -4,9 +4,9 @@ function UserDetails(props) {
     const user = props.user
     return (
         <li key={user.id} className="data">
-            <span className="italics"> Name:</span> {user?.name === "None" ? "N/A" : user.name},
-            <span className="italics"> Age:</span> {user?.age === "None" ? "N/A" : user.age},
-            <span className="italics"> Favorite Color:</span> {user?.favorite_color === "None" ? "N/A" : user.favorite_color}
+            <span className="italics"> Name:</span> {user?.name === null ? "N/A" : user.name},
+            <span className="italics"> Age:</span> {user?.age === null ? "N/A" : user.age},
+            <span className="italics"> Favorite Color:</span> {user?.favorite_color === null ? "N/A" : user.favorite_color}
         </li>
     )
 };
