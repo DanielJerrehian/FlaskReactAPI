4d7ffb61452fd90bfd6dada26b0ad195b61ab651
Daniel Jerrehian <d.jerrehian@kigroup.de>
2022-04-30 17:23:18 +0200

    added pytest & conftest to properly test flask app

--- app/server/src/app.py
+++ app/server/src/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
+from src.network.cors import cors
 from src.models.db import db
 from src.models.marshmallow import ma
 from src.routers.index import main
@@ -10,7 +10,7 @@ from src.routers.index import main
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    
     load_dotenv(dotenv_path=".env")
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
     app.config["ENV"] = "development"
@@ -19,6 +19,7 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///models/database.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    cors.init_app(app)
     db.init_app(app)
     ma.init_app(app)
     
--- app/server/tests/test_logic/test_site_data/test_site_data.py
+++ app/server/tests/test_logic/test_site_data/test_site_data.py
@@ -1,62 +1,52 @@
-import unittest
-
-from src.app import create_app
 from src.logic.site_data.site_data import SiteData
 
-
-class TestSiteData(unittest.TestCase):
-    def setUp(self):
-        self.app = create_app()
     
-    def test_class_exists(self):
-        site_data = SiteData
-        self.assertTrue(site_data)
+def test_class_exists():
+    site_data = SiteData
+    assert site_data
 
-    def test_class_vars(self):
-        site_data = SiteData()
-        self.assertIsNone(site_data.project_name)
-        self.assertIsNone(site_data.user_count)
-        self.assertIsNone(site_data.favorite_color)
-        self.assertIsNone(site_data.second_favorite_color)
-        self.assertIsNone(site_data.average_age)
-        self.assertIsNone(site_data.last_three_users)
+def test_class_vars():
+    site_data = SiteData()
+    assert site_data.project_name == None
+    assert site_data.user_count == None
+    assert site_data.favorite_color == None
+    assert site_data.second_favorite_color == None
+    assert site_data.average_age == None
+    assert site_data.last_three_users == None
         
-    def test_get_project_name(self):
-        site_data = SiteData()
-        site_data.get_project_name()
-        self.assertEqual(site_data.project_name, "FlaskReactAPI")
+def test_get_project_name():
+    site_data = SiteData()
+    site_data.get_project_name()
+    assert site_data.project_name == "FlaskReactAPI"
         
-    def test_count_user(self):
-        with self.app.app_context():
-            site_data = SiteData()
-            site_data.count_user()
-            self.assertEqual(site_data.user_count, 14)
+def test_count_user(app):
+    with app.app_context():
+        site_data = SiteData()
+        site_data.count_user()
+        assert site_data.user_count == 14
             
-    def test_get_favorite_color(self):
-        with self.app.app_context():
-            site_data = SiteData()
-            site_data.get_favorite_color()
-            self.assertEqual(site_data.favorite_color, "Purple")
+def test_get_favorite_color(app):
+    with app.app_context():
+        site_data = SiteData()
+        site_data.get_favorite_color()
+        assert site_data.favorite_color == "Purple"
 
-    def test_get_favorite_color(self):
-        with self.app.app_context():
-            site_data = SiteData()
-            site_data.get_second_favorite_color()
-            self.assertEqual(site_data.second_favorite_color, "Red")
+def test_get_favorite_color(app):
+    with app.app_context():
+        site_data = SiteData()
+        site_data.get_second_favorite_color()
+        assert site_data.second_favorite_color == "Red"
 
-    def test_average_user_age(self):
-        with self.app.app_context():
-            site_data = SiteData()
-            site_data.average_user_age()
-            self.assertEqual(round(site_data.average_age, 0), 28)
+def test_average_user_age(app):
+    with app.app_context():
+        site_data = SiteData()
+        site_data.average_user_age()
+        assert round(site_data.average_age, 0) == 28
 
-    def test_fetch_last_three_users(self):
-        with self.app.app_context():
-            site_data = SiteData()
-            site_data.fetch_last_three_users()
-            self.assertEqual(site_data.last_three_users[0].name, "Ruben")
-            self.assertEqual(site_data.last_three_users[1].name, "Aurelien")
-            self.assertEqual(site_data.last_three_users[2].name, "Cynthia")
-            
-    def tearDown(self):
-        pass
+def test_fetch_last_three_users(app):
+    with app.app_context():
+        site_data = SiteData()
+        site_data.fetch_last_three_users()
+        assert site_data.last_three_users[0].name == "Ruben"
+        assert site_data.last_three_users[1].name == "Aurelien"
+        assert site_data.last_three_users[2].name == "Cynthia"
